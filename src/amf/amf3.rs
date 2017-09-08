ee8849570798d21330af0236a752cfba7673ae95
PyYoshi <myoshi321go@gmail.com>
2017-09-09 01:08:00 +0900

    AMF3: Decoder構造体を実装

--- src/amf/amf3.rs
+++ src/amf/amf3.rs
@@ -43,196 +43,216 @@ pub enum Value {
     Xml(String),
 }
 
-// 1.3.1 Variable Length Unsigned 29-bit Integer Encoding
-// AMF 3 makes use of a special compact format for writing integers to reduce the number
-// of bytes required for encoding. As with a normal 32-bit integer, up to 4 bytes are required
-// to hold the value however the high bit of the first 3 bytes are used as flags to determine
-// whether the next byte is part of the integer. With up to 3 bits of the 32 bits being used as
-// flags, only 29 significant bits remain for encoding an integer. This means the largest
-// unsigned integer value that can be represented is 229 - 1.
-// (hex) : (binary)
-// 0x00000000 - 0x0000007F : 0xxxxxxx
-// 0x00000080 - 0x00003FFF : 1xxxxxxx 0xxxxxxx
-// 0x00004000 - 0x001FFFFF : 1xxxxxxx 1xxxxxxx 0xxxxxxx
-// 0x00200000 - 0x3FFFFFFF : 1xxxxxxx 1xxxxxxx 1xxxxxxx xxxxxxxx
-// 0x40000000 - 0xFFFFFFFF : throw range exception
-// In ABNF syntax, the variable length unsigned 29-bit integer type is described as follows:
-// U29 = U29-1 | U29-2 | U29-3 | U29-4
-// U29-1 = %x00-7F
-// U29-2 = %x80-FF %x00-7F
-// U29-3 = %x80-FF %x80-FF %x00-7F
-// U29-4 = %x80-FF %x80-FF %x80-FF %x00-FF
-fn decode_u29<R: io::Read>(reader: &mut R) -> DecodeResult<u32> {
-    let mut n = 0;
-    for _ in 0..3 {
-        let b = try!(reader.read_u8()) as u32;
-        n = (n << 7) | (b & 0x7f);
-        if (b & 0x80) == 0 {
-            return Ok(n);
+#[derive(Debug)]
+pub struct Decoder<R> {
+    reader: R,
+    objects: Vec<Value>,
+}
+
+impl<R> Decoder<R>
+where
+    R: io::Read,
+{
+    pub fn new(reader: R) -> Self {
+        Decoder {
+            reader: reader,
+            objects: Vec::new(),
         }
     }
-    let b = try!(reader.read_u8()) as u32;
-    n = (n << 8) | b;
-    Ok(n)
-}
 
-fn decode_integer<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    let num = try!(decode_u29(reader)) as i32;
-    let num = if num >= (1 << 28) {
-        num - (1 << 29)
-    } else {
-        num
-    };
-    Ok(Value::Integer(num))
-}
-
-fn decode_double<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    let n = try!(reader.read_f64::<BigEndian>());
-    Ok(Value::Double(n))
-}
-
-fn read_bytes<R: io::Read>(reader: &mut R, len: usize) -> DecodeResult<Vec<u8>> {
-    let mut buf = vec![0; len];
-    try!(reader.read_exact(&mut buf));
-    Ok(buf)
-}
-
-fn decode_utf8<R: io::Read>(reader: &mut R) -> DecodeResult<String> {
-    let u29 = try!(decode_u29(reader)) as usize;
-    let is_reference = (u29 & 0x1) == 0;
-    let value = u29 >> 1;
-    if is_reference {
-        Err(DecodeError::NotSupportedReferenceTables { index: value })
-    } else {
-        let bytes = try!(read_bytes(reader, value));
-        let s = try!(String::from_utf8(bytes));
-        Ok(s)
+    pub fn decode(&mut self) -> DecodeResult<Value> {
+        self.objects.clear();
+        self.decode_value()
     }
-}
 
-fn decode_string<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    let s = try!(decode_utf8(reader));
-    Ok(Value::String(s))
-}
-
-fn decode_xml_doc<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    let s = try!(decode_utf8(reader));
-    Ok(Value::XmlDoc(s))
-}
-fn decode_date<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    try!(decode_u29(reader)) as usize; // skip
-    let millis = try!(reader.read_f64::<BigEndian>());
-    Ok(Value::Date {
-        unixtime: time::Duration::from_millis(millis as u64),
-    })
-}
-
-fn decode_pairs<R: io::Read>(reader: &mut R) -> DecodeResult<Vec<Pair<String, Value>>> {
-    let mut pairs = Vec::new();
-    loop {
-        let key = try!(decode_utf8(reader));
-        if key.is_empty() {
-            return Ok(pairs);
+    // 1.3.1 Variable Length Unsigned 29-bit Integer Encoding
+    // AMF 3 makes use of a special compact format for writing integers to reduce the number
+    // of bytes required for encoding. As with a normal 32-bit integer, up to 4 bytes are required
+    // to hold the value however the high bit of the first 3 bytes are used as flags to determine
+    // whether the next byte is part of the integer. With up to 3 bits of the 32 bits being used as
+    // flags, only 29 significant bits remain for encoding an integer. This means the largest
+    // unsigned integer value that can be represented is 229 - 1.
+    // (hex) : (binary)
+    // 0x00000000 - 0x0000007F : 0xxxxxxx
+    // 0x00000080 - 0x00003FFF : 1xxxxxxx 0xxxxxxx
+    // 0x00004000 - 0x001FFFFF : 1xxxxxxx 1xxxxxxx 0xxxxxxx
+    // 0x00200000 - 0x3FFFFFFF : 1xxxxxxx 1xxxxxxx 1xxxxxxx xxxxxxxx
+    // 0x40000000 - 0xFFFFFFFF : throw range exception
+    // In ABNF syntax, the variable length unsigned 29-bit integer type is described as follows:
+    // U29 = U29-1 | U29-2 | U29-3 | U29-4
+    // U29-1 = %x00-7F
+    // U29-2 = %x80-FF %x00-7F
+    // U29-3 = %x80-FF %x80-FF %x00-7F
+    // U29-4 = %x80-FF %x80-FF %x80-FF %x00-FF
+    fn decode_u29(&mut self) -> DecodeResult<u32> {
+        let mut n = 0;
+        for _ in 0..3 {
+            let b = try!(self.reader.read_u8()) as u32;
+            n = (n << 7) | (b & 0x7f);
+            if (b & 0x80) == 0 {
+                return Ok(n);
+            }
         }
-        let value = try!(decode_value(reader));
-        pairs.push(Pair {
-            key: key,
-            value: value,
-        });
+        let b = try!(self.reader.read_u8()) as u32;
+        n = (n << 8) | b;
+        Ok(n)
     }
-}
 
-fn decode_object<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    let u29 = try!(decode_u29(reader)) as usize;
-    let is_reference = (u29 & 0x1) == 0;
-    let value = u29 >> 1;
-    if is_reference {
-        Err(DecodeError::NotSupportedReferenceTables { index: value })
-    } else {
-        if (value & 0x1) == 0 {
+    fn read_bytes(&mut self, len: usize) -> DecodeResult<Vec<u8>> {
+        let mut buf = vec![0; len];
+        try!(self.reader.read_exact(&mut buf));
+        Ok(buf)
+    }
+
+    fn decode_utf8(&mut self) -> DecodeResult<String> {
+        let u29 = try!(self.decode_u29()) as usize;
+        let is_reference = (u29 & 0x1) == 0;
+        let value = u29 >> 1;
+        if is_reference {
             Err(DecodeError::NotSupportedReferenceTables { index: value })
         } else {
-            let class_ref = value >> 0x1;
-            if (class_ref & 0x1) == 0 {
-                let class_index = class_ref >> 0x1;
-                Err(DecodeError::NotSupportedReferenceTables {
-                    index: class_index,
-                })
+            let bytes = try!(self.read_bytes(value));
+            let s = try!(String::from_utf8(bytes));
+            Ok(s)
+        }
+    }
+
+    fn decode_pairs(&mut self) -> DecodeResult<Vec<Pair<String, Value>>> {
+        let mut pairs = Vec::new();
+        loop {
+            let key = try!(self.decode_utf8());
+            if key.is_empty() {
+                return Ok(pairs);
+            }
+            let value = try!(self.decode_value());
+            pairs.push(Pair {
+                key: key,
+                value: value,
+            });
+        }
+    }
+
+    fn decode_integer(&mut self) -> DecodeResult<Value> {
+        let num = try!(self.decode_u29()) as i32;
+        let num = if num >= (1 << 28) {
+            num - (1 << 29)
+        } else {
+            num
+        };
+        Ok(Value::Integer(num))
+    }
+
+    fn decode_double(&mut self) -> DecodeResult<Value> {
+        let n = try!(self.reader.read_f64::<BigEndian>());
+        Ok(Value::Double(n))
+    }
+
+    fn decode_string(&mut self) -> DecodeResult<Value> {
+        let s = try!(self.decode_utf8());
+        Ok(Value::String(s))
+    }
+
+    fn decode_xml_doc(&mut self) -> DecodeResult<Value> {
+        let s = try!(self.decode_utf8());
+        Ok(Value::XmlDoc(s))
+    }
+    fn decode_date(&mut self) -> DecodeResult<Value> {
+        try!(self.decode_u29()) as usize; // skip
+        let millis = try!(self.reader.read_f64::<BigEndian>());
+        Ok(Value::Date {
+            unixtime: time::Duration::from_millis(millis as u64),
+        })
+    }
+
+    fn decode_object(&mut self) -> DecodeResult<Value> {
+        let u29 = try!(self.decode_u29()) as usize;
+        let is_reference = (u29 & 0x1) == 0;
+        let value = u29 >> 1;
+        if is_reference {
+            Err(DecodeError::NotSupportedReferenceTables { index: value })
+        } else {
+            if (value & 0x1) == 0 {
+                Err(DecodeError::NotSupportedReferenceTables { index: value })
             } else {
-                let class_ext_ref = class_ref >> 0x1;
-
-                let is_externalizable = (class_ext_ref & 0x1) == 1;
-                let is_dynamic = ((class_ext_ref >> 0x1) & 0x1) == 1;
-
-                let cdnum = class_ext_ref >> 0x2;
-
-                let class_name = try!(decode_utf8(reader));
-                let fields: Vec<String> = try!((0..cdnum).map(|_| decode_utf8(reader)).collect());
-
-                if is_externalizable {
-                    let class_name = try!(decode_utf8(reader));
-                    Err(DecodeError::ExternalizableType { name: class_name })
-                } else {
-                    let mut pairs = try!(
-                        fields
-                            .iter()
-                            .map(|k| {
-                                Ok(Pair {
-                                    key: k.clone(),
-                                    value: try!(decode_value(reader)),
-                                })
-                            })
-                            .collect::<DecodeResult<Vec<_>>>()
-                    );
-
-                    if is_dynamic {
-                        pairs.extend(try!(decode_pairs(reader)));
-                    }
-
-                    Ok(Value::Object {
-                        name: Some(class_name),
-                        pairs: pairs,
+                let class_ref = value >> 0x1;
+                if (class_ref & 0x1) == 0 {
+                    let class_index = class_ref >> 0x1;
+                    Err(DecodeError::NotSupportedReferenceTables {
+                        index: class_index,
                     })
+                } else {
+                    let class_ext_ref = class_ref >> 0x1;
+
+                    let is_externalizable = (class_ext_ref & 0x1) == 1;
+                    let is_dynamic = ((class_ext_ref >> 0x1) & 0x1) == 1;
+
+                    let cdnum = class_ext_ref >> 0x2;
+
+                    let class_name = try!(self.decode_utf8());
+                    let fields: Vec<String> =
+                        try!((0..cdnum).map(|_| self.decode_utf8()).collect());
+
+                    if is_externalizable {
+                        let class_name = try!(self.decode_utf8());
+                        Err(DecodeError::ExternalizableType { name: class_name })
+                    } else {
+                        let mut pairs = try!(
+                            fields
+                                .iter()
+                                .map(|k| {
+                                    Ok(Pair {
+                                        key: k.clone(),
+                                        value: try!(self.decode_value()),
+                                    })
+                                })
+                                .collect::<DecodeResult<Vec<_>>>()
+                        );
+
+                        if is_dynamic {
+                            pairs.extend(try!(self.decode_pairs()));
+                        }
+
+                        Ok(Value::Object {
+                            name: Some(class_name),
+                            pairs: pairs,
+                        })
+                    }
                 }
             }
         }
     }
-}
 
-fn decode_xml<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    let s = try!(decode_utf8(reader));
-    Ok(Value::Xml(s))
-}
-
-fn decode_value<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    let marker = try!(reader.read_u8());
-    match marker {
-        Marker::UNDEFINED => Ok(Value::Undefined),
-        Marker::NULL => Ok(Value::Null),
-        Marker::FALSE => Ok(Value::Boolean(false)),
-        Marker::TRUE => Ok(Value::Boolean(true)),
-        Marker::INTEGER => decode_integer(reader),
-        Marker::DOUBLE => decode_double(reader),
-        Marker::STRING => decode_string(reader),
-        Marker::XML_DOC => decode_xml_doc(reader),
-        Marker::DATE => decode_date(reader),
-        Marker::OBJECT => decode_object(reader),
-        Marker::XML => decode_xml(reader),
-
-        Marker::ARRAY => Err(DecodeError::NotSupportedType { marker }),
-        Marker::BYTE_ARRAY => Err(DecodeError::NotSupportedType { marker }),
-        Marker::VECTOR_INT => Err(DecodeError::NotSupportedType { marker }),
-        Marker::VECTOR_UINT => Err(DecodeError::NotSupportedType { marker }),
-        Marker::VECTOR_DOUBLE => Err(DecodeError::NotSupportedType { marker }),
-        Marker::VECTOR_OBJECT => Err(DecodeError::NotSupportedType { marker }),
-        Marker::DICTIONARY => Err(DecodeError::NotSupportedType { marker }),
-
-        _ => Err(DecodeError::UnknownType { marker }),
+    fn decode_xml(&mut self) -> DecodeResult<Value> {
+        let s = try!(self.decode_utf8());
+        Ok(Value::Xml(s))
     }
-}
 
-pub fn decode<R: io::Read>(reader: &mut R) -> DecodeResult<Value> {
-    decode_value(reader)
+    fn decode_value(&mut self) -> DecodeResult<Value> {
+        let marker = try!(self.reader.read_u8());
+        match marker {
+            Marker::UNDEFINED => Ok(Value::Undefined),
+            Marker::NULL => Ok(Value::Null),
+            Marker::FALSE => Ok(Value::Boolean(false)),
+            Marker::TRUE => Ok(Value::Boolean(true)),
+            Marker::INTEGER => self.decode_integer(),
+            Marker::DOUBLE => self.decode_double(),
+            Marker::STRING => self.decode_string(),
+            Marker::XML_DOC => self.decode_xml_doc(),
+            Marker::DATE => self.decode_date(),
+            Marker::OBJECT => self.decode_object(),
+            Marker::XML => self.decode_xml(),
+
+            Marker::ARRAY => Err(DecodeError::NotSupportedType { marker }),
+            Marker::BYTE_ARRAY => Err(DecodeError::NotSupportedType { marker }),
+            Marker::VECTOR_INT => Err(DecodeError::NotSupportedType { marker }),
+            Marker::VECTOR_UINT => Err(DecodeError::NotSupportedType { marker }),
+            Marker::VECTOR_DOUBLE => Err(DecodeError::NotSupportedType { marker }),
+            Marker::VECTOR_OBJECT => Err(DecodeError::NotSupportedType { marker }),
+            Marker::DICTIONARY => Err(DecodeError::NotSupportedType { marker }),
+
+            _ => Err(DecodeError::UnknownType { marker }),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -242,13 +262,17 @@ mod test {
     use std::time;
 
     use super::Value;
-    use super::decode;
+    use super::Decoder;
 
     macro_rules! macro_decode {
         ($sample_file: expr) => {
             {
-                let mut reader = BufReader::new(fs::File::open(concat!("./testdata/", $sample_file)).unwrap());
-                decode(&mut reader)
+                let mut decoder = Decoder::new(
+                    BufReader::new(fs::File::open(
+                        concat!("./testdata/", $sample_file)).unwrap()
+                    )
+                );
+                decoder.decode()
             }
         }
     }
